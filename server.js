b2d89322a3a91c9716bd4b762ce1e734c8056828
nicolaile <nicolai.le2@gmail.com>
2017-09-22 15:21:57 +0200

    le

--- server.js
+++ server.js
@@ -2,11 +2,11 @@ var httpServer = require('./servers/http'),
     resources = require('./resources/model');
 
 var dhtPlugin = require('./plugins/DHT22SensorPlugin');
-//var ledPlugin = require('./plugins/ledsPlugin');
+var ledPlugin = require('./plugins/ledsPlugin');
 
 
 dhtPlugin.start({'simulate': true, 'frequency': 10000});
-//ledPlugin.start({'simulate':false});
+ledPlugin.start({'simulate':false});
 
 var server = httpServer.listen(resources.pi.port, function () {
     console.info('RaspberryPi 3 is running on port %s',
