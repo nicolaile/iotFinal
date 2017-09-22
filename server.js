5ba10707e27e9ee9b29319e567c767c2513e0dc1
nicolaile <nicolai.le2@gmail.com>
2017-09-22 09:23:31 +0200

    jj
    nn

--- server.js
+++ server.js
@@ -6,6 +6,6 @@ var dhtPlugin = require('./plugins/DHT22SensorPlugin');
 dhtPlugin.start({'simulate': false, 'frequency': 10000});
 
 var server = httpServer.listen(resources.pi.port, function () {
-    console.info('RaspberryPi is running on port %s',
+    console.info('RaspberryPi 3 is running on port %s',
     resources.pi.port);
     });
