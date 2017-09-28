4747fba4ff50ad0245b56ec3873373daa1c52456
nicolaile <nicolai.le2@gmail.com>
2017-09-28 13:57:32 +0200

    ds

--- routes/actuators.js
+++ routes/actuators.js
@@ -1,18 +1,18 @@
 var express = require('express'),
-  router = express.Router(), //#A
+  router = express.Router(),
   resources = require('./../resources/model');
 var server = require('./../server');
 
-  router.route('/').get(function (req, res, next) { // #A
-   res.send(resources.pi.actuators); // #B
+  router.route('/').get(function (req, res, next) {
+   res.send(resources.pi.actuators);
   });
 
-  router.route('/leds').get(function (req, res, next) { // #C
+  router.route('/leds').get(function (req, res, next) {
     res.send(resources.pi.actuators.leds);
   });
 
-  router.route('/leds/:id').get(function (req, res, next) { //#D
-    res.send(resources.pi.actuators.leds[req.params.id]); //#E
+  router.route('/leds/:id').get(function (req, res, next) {
+    res.send(resources.pi.actuators.leds[req.params.id]);
   }).put(function(req,res,next){
 
     var myLed = resources.pi.actuators.leds[req.params.id];
