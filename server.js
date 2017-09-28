8b03c499380819d3470cd6e835a10a46e1528c68
nicolaile <nicolai.le2@gmail.com>
2017-09-28 13:26:15 +0200

    sad

--- server.js
+++ server.js
@@ -4,11 +4,45 @@ var httpServer = require('./servers/http'),
 var dhtPlugin = require('./plugins/DHT22SensorPlugin');
 var ledPlugin = require('./plugins/ledsPlugin');
 
+var Gpio = require('onoff').Gpio;
+actuator = new Gpio(model.gpio, 'out'); //#D
 
 dhtPlugin.start({'simulate': true, 'frequency': 10000});
-ledPlugin.start({'simulate':false});
+//ledPlugin.start({'simulate':false});
 
 var server = httpServer.listen(resources.pi.port, function () {
     console.info('RaspberryPi 3 is running on port %s',
     resources.pi.port);
     });
+
+
+
+
+    var actuator, interval;
+    var model = resources.pi.actuators.leds['1'];
+    var pluginName = model.name;
+    var localParams = {'simulate': false, 'frequency': 2000};
+
+
+    /*function observe(what) {
+      Object.observe(what, function (changes) {
+        console.info('Change detected by plugin for %s...', pluginName);
+        switchOnOff(model.value); //#B
+      });
+    };
+    */
+    var switchOnOff = function() {
+        actuator.write(value === true ? 1 : 0, function () { //#C
+          console.info('Changed value of %s to %s', pluginName, value);
+        });
+
+    };
+
+
+    //  console.info('Hardware %s actuator started!' + actuator.readSync(), pluginName);
+
+
+
+    module.exports = {
+      switchOnOff
+    };
